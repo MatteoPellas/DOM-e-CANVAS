ec8e963259a5c1c0360d417b483beab980ff5297
MatteoPellas <pellas.matteo@iisgalvanimi.edu.it>
2021-11-08 12:48:44 +0000

    primo commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function button1Function() {
+    document.getElementById("mytext").innerHTML = "a me piace javascript";
+   
+  }
+function button2Function(){
+    document.getElementById("mytext2").innerHTML = "a me non piace javascript";
+
+}  
+  
+  
